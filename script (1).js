ac8d1f4f457d993126fc3f604c7d0ebf905620f6
NandhaKumar-AE <140687963+NandhaKumar-AE@users.noreply.github.com>
2024-07-18 18:52:39 +0530

    Delete script (1).js

--- script (1).js
+++
@@ -1,9 +0,0 @@
-let currentIndex = 1;
-  const totalItems = 3;
-  const intervalTime = 5000; //5 sec
-
-  setInterval(() => {
-    document.getElementById(`item-${currentIndex}`).checked = false;
-    currentIndex = currentIndex % totalItems + 1;
-    document.getElementById(`item-${currentIndex}`).checked = true;
-  }, intervalTime);
